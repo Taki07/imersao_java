7a965b1d53a33285953834b4deee687bb37e89aa
Taki07 <48402366+Taki07@users.noreply.github.com>
2022-07-18 19:00:02 -0300

    Imersão Java - Imdb API

---
+++ Imersão-Java-Imdb-API/src/APIKey.java
@@ -0,0 +1,5 @@
+public class APIKey {
+
+    public static String Chave = "k_2de8ar4z";
+    
+}
---
+++ Imersão-Java-Imdb-API/src/App.java
@@ -0,0 +1,59 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("\u001b[1m Bem-vindo! O que vamos assistir hoje? \u001b[0m");
+    
+    // Pegar os dados do banco de dados do Imdb com http
+    
+    // Top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/";
+        
+            URI endereço = URI.create(url + APIKey.Chave);
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder(endereço).GET().build();
+                HttpResponse<String> resposta =client.send(request, BodyHandlers.ofString());
+                    String retorno = resposta.body();
+                
+    // Melhores séries
+        String url2 = "https://imdb-api.com/en/API/MostPopularTVs/";
+            URI endereço2 = URI.create(url2 + APIKey.Chave);
+                request = HttpRequest.newBuilder(endereço2).GET().build();
+                HttpResponse<String> resposta2 = client.send(request, BodyHandlers.ofString());
+                    String  retorno2 = resposta2.body();
+                
+    // Tratar os dados vindos do site por Json (título, poster, classificação)
+     JsonParser parser = new JsonParser();
+        List<Map<String, String>> Lista = parser.parse(retorno);
+        List<Map<String, String>> Lista2 = parser.parse(retorno2);
+    
+    // Visualização dos dados obtidos pelo site
+        for (Map<String,String> filme : Lista) {
+            
+            System.out.println("\u001b[1m Nome: " + filme.get("title")+ " \u001b[0m");
+            System.out.println("\u001b[3m Pôster: " + filme.get("image") + "\u001b[0m");
+            System.out.println("\u001b[45m Nota imDb: " + filme.get("imDbRating")+ "⭐" + "\u001b[0m");
+            System.out.println();
+        }
+                for (Map<String,String> serie : Lista2) {
+                    System.out.println("\u001b[1m Nome:" + serie.get("title") + "\u001b[0m");
+                    System.out.println("\u001b[3m Pôster:" + serie.get("image") + "\u001b[0m");
+                    System.out.println("\u001b[45m Nota imDb:" + serie.get("imDbRating")+ "⭐" +  "\u001b[0m");
+                    System.out.println();
+                }
+
+    // Exportar os dados como Stickers pra Wpp, Telegram
+
+    
+    
+    }
+
+
+}
